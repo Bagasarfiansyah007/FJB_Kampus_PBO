e88ac8b8810bd8d4b72554fbc584ab5f4a73bc3a
ridhoaanhrp <55271500+ridhoaanhrp@users.noreply.github.com>
2021-04-01 20:36:56 +0700

    Update Penjual.java

--- Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
+++ Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
@@ -3,5 +3,42 @@ package Entitas;
 
 
 public class Penjual {
+    private int produk_terjual;
     
+    public void setPenjual(){
+        int nim;
+        String nama,fakultas,prodi;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Nama Mahasiswa     : ");
+        nama = input.nextLine();
+        super.setNama(nama);
+        
+        System.out.print("NIM Mahasiswa      : ");
+        nim = input.nextInt();
+        super.setNim(nim);
+        
+        fakultas = input.nextLine();
+        
+        System.out.print("Fakultas Mahasiswa : ");
+        fakultas = input.nextLine();
+        super.setFakultas(fakultas);
+        
+        System.out.print("Prodi Mahasiswa    : ");
+        prodi = input.nextLine();
+        super.setProdi(prodi);
+    }
+    public void menjual(){
+        
+    }
+    public void konfirmasi(){
+        
+    }
+    public void info(){
+        System.out.println(" ");
+        System.out.println("NIM Mahasiswa      : " + super.getNim());
+        System.out.println("Nama Mahasiswa     : " + super.getNama());
+        System.out.println("Fakultas Mahasiswa : " + super.getfakultas());
+        System.out.println("Prodi Mahasiswa    : " + super.getProdi());
+    }
 }
