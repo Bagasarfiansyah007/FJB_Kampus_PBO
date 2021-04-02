54ed4b23d2c555afd80b966e5f5e411030ffc20d
zahid al muhasibi <55351989+zahi8@users.noreply.github.com>
2021-04-02 21:24:48 +0700

    Update Chcekout.java

--- Tugas_besar_FJB-Campus/src/Relasi/Chcekout.java
+++ Tugas_besar_FJB-Campus/src/Relasi/Chcekout.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Relasi;
 
-/**
- *
- * @author AMS
- */
 public class Chcekout extends User{
     private String no_order;
     private int jumlah_order;
