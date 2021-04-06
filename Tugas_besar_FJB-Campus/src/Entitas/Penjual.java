55d98f86b33a147dc231790c5dbfc162fa38184c
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-06 16:50:57 +0700

    Update Penjual.java

--- Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
+++ Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
@@ -1,53 +1,23 @@
+
 package Entitas;
+
 import java.util.Scanner;
+
 
 public class Penjual extends User {
     private int Produk_terjual;
     private int saldo_penjualan;
-    private String prodi;
     
-    public void setUser(){
-        int nim;
-        String nama,fakultas,prodi,username,password;
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Nama Mahasiswa     : ");
-        nama = input.nextLine();
-        super.setNama(nama);
-        
-        System.out.print("NIM Mahasiswa      : ");
-        nim = input.nextInt();
-        super.setNim(nim);
-        
-        fakultas = input.nextLine();
-        
-        System.out.print("Fakultas Mahasiswa : ");
-        fakultas = input.nextLine();
-        super.setFakultas(fakultas);
-        
-        System.out.print("Prodi Mahasiswa    : ");
-        prodi = input.nextLine();
-        super.setProdi(prodi);
-        
-        System.out.print("username mahasiswa : ");
-        username = input.nextLine();
-        super.setUsername(username);
-        
-        System.out.print("username mahasiswa : ");
-        password = input.nextLine();
-        super.setPassword(password);
+    
+    public void tampilanMenuPenjual (User[] tableUser,int indeks){
+        System.out.println("-----------------------------------------------");
+        System.out.println("|            Login Sebagai Penjual            |");
+        System.out.println("-----------------------------------------------");
+        System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "   |");
+        System.out.println("-----------------------------------------------");
+        System.out.println("1.Input User");
+        System.out.println("2.Tampil data");
+        System.out.println("0.Keluar");
+        System.out.println(" ");   
     }
-    public void menjual(){
-        
-    }
-    public void konfirmasi(){
-        
-    }
-    public void info(){
-        System.out.println(" ");
-        System.out.println("NIM Mahasiswa      : " + super.getNim());
-        System.out.println("Nama Mahasiswa     : " + super.getNama());
-        System.out.println("Fakultas Mahasiswa : " + super.getfakultas());
-        System.out.println("Prodi Mahasiswa    : " + super.getProdi());
-    }   
 }
