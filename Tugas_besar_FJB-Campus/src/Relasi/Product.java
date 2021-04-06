fca946468f1e177797b5063d0f2f6f5e7f7d49a3
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-06 19:01:54 +0700

    Update Product.java

--- Tugas_besar_FJB-Campus/src/Relasi/Product.java
+++ Tugas_besar_FJB-Campus/src/Relasi/Product.java
@@ -82,6 +82,23 @@ public class Product extends Penjual{
         i++;
     }
     
+    
+    public String getIdbarang() {
+        return id_barang;
+    }
+    
+    public String getNamaBarang() {
+        return nama;
+    }
+    
+    public String getKategori() {
+        return kategori;
+    } 
+    
+    public int getHarga() {
+        return Harga;
+    } 
+  
     public void inputDataProductJual(Product[] tableProduct,int indeksglobal,User[] tableUser,int indekscari){
         indeksglobal = tableProduct[0].getValue(tableProduct);
         tableProduct[indeksglobal] = new Product();
@@ -96,4 +113,43 @@ public class Product extends Penjual{
         
         return banyak;
     }
+    
+    public void info(Product[] tableProduct){
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("|                           Produk Tersedia                            |");        
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("| Pilih Kategori | 1.Elektronik | 2.Fashion | 3.kecantikan | 4.Makanan |");
+        System.out.println("------------------------------------------------------------------------");
+        for (int i = 0; i < tableProduct[0].getValue(tableProduct); i++) {
+            System.out.println("|                | Id Barang          : " + tableProduct[i].getIdbarang());
+            System.out.println("|                | nim mahasiswa      : " + tableProduct[i].getNim());
+            System.out.println("|                | Nama mahasiswa     : " + tableProduct[i].getNama());
+            System.out.println("|        "+ (i + 1) +"       | Nama Barang        : " + tableProduct[i].getNamaBarang());
+            System.out.println("|                | No Telphone        : " + tableProduct[i].getNo());
+            System.out.println("|                | Kategori Barang    : " + tableProduct[i].getKategori());
+            System.out.println("|                | Harga Barang       : " + tableProduct[i].getHarga());
+            System.out.println("------------------------------------------------------------------------");
+        }
+    }
+    
+    public void setUserManual(int nim,String nama,String fakultas,String noTelpon,String prodi,String id,String namabarang,String kategori,int harga){
+        super.setNim(nim);
+        super.setNama(nama);
+        super.setFakultas(fakultas);
+        super.setProdi(prodi);
+        super.setNo(noTelpon);
+        this.nama = namabarang;
+        this.Harga = harga;
+        this.kategori = kategori;
+        this.id_barang = id;
+    }
+    
+    public void inputdatapreview (Product[] tableProduct){
+        for (int i = 1 ; i < 2 ; i ++) {
+            tableProduct[i] = new Product();
+        }
+        tableProduct[0].setUserManual(1302194007,"Bagas Arfiansyah","FIF","089119231492","RPL","PRD01","Tote Bag","Fashion",10000);
+        tableProduct[1].setUserManual(1302191000,"Erlangga saputro","FKB","082144412433","Akutansi","PRD02","Iphone 11 pro","Elektronik",2000000);
+    }
 }
+ 
