be3c49cc39c70d734da0bc2069c2e78bfbf370e8
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-08 00:23:38 +0700

    Update Penjual.java

--- Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
+++ Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
@@ -15,9 +15,12 @@ public class Penjual extends User {
         System.out.println("-----------------------------------------------");
         System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "   |");
         System.out.println("-----------------------------------------------");
-        System.out.println("1.Input User");
-        System.out.println("2.Tampil data");
-        System.out.println("0.Keluar");
+        System.out.println("| 1.Input data Barang                         |");
+        System.out.println("| 2.Update data Barang                        |");
+        System.out.println("| 3.Tampil Product                            |");
+        System.out.println("| 0.Keluar                                    |");
+        System.out.println("-----------------------------------------------");
         System.out.println(" ");   
     }
+    
 }
