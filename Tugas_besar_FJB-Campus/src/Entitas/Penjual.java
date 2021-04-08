228d2aef7799338a0080bef5905afa3419572e6f
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-08 09:18:28 +0700

    Update Penjual.java

--- Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
+++ Tugas_besar_FJB-Campus/src/Entitas/Penjual.java
@@ -1,26 +1,22 @@
-
 package Entitas;
-
 import java.util.Scanner;
 
 
-public class Penjual extends User {
-    private int Produk_terjual;
-    private int saldo_penjualan;
+public class Pembeli extends User{
+    private int saldo;
+    private int barangTerbeli;
     
-    
-    public void tampilanMenuPenjual (User[] tableUser,int indeks){
+    public void tampilMenuPembeli(User[] tableUser,int indeks){
         System.out.println("-----------------------------------------------");
-        System.out.println("|            Login Sebagai Penjual            |");
+        System.out.println("|            Login Sebagai Pembeli            |");
         System.out.println("-----------------------------------------------");
-        System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "   |");
+        System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "    |");
         System.out.println("-----------------------------------------------");
-        System.out.println("| 1.Input data Barang                         |");
-        System.out.println("| 2.Update data Barang                        |");
-        System.out.println("| 3.Tampil Product                            |");
-        System.out.println("| 0.Keluar                                    |");
-        System.out.println("-----------------------------------------------");
-        System.out.println(" ");   
+        System.out.println("1.Beli Barang");
+        System.out.println("2.Checkout");
+        System.out.println("3.Status pembayaran");
+        System.out.println("4.Update account");
+        System.out.println("0.Keluar");
+        System.out.println(" ");
     }
-    
 }
