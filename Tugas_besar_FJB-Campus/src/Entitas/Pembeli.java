2b8dba5e91447746f71f2145763036ae32489a94
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-08 09:33:26 +0700

    Update Pembeli.java

--- Tugas_besar_FJB-Campus/src/Entitas/Pembeli.java
+++ Tugas_besar_FJB-Campus/src/Entitas/Pembeli.java
@@ -1,22 +1,42 @@
+
 package Entitas;
+
 import java.util.Scanner;
 
 
-public class Pembeli extends User{
-    private int saldo;
-    private int barangTerbeli;
+public class Penjual extends User {
+    private int Produk_terjual;
+    private int saldo_penjualan;
     
-    public void tampilMenuPembeli(User[] tableUser,int indeks){
+    
+    public void tampilanMenuPenjual (User[] tableUser,int indeks){
         System.out.println("-----------------------------------------------");
-        System.out.println("|            Login Sebagai Pembeli            |");
+        System.out.println("|            Login Sebagai Penjual            |");
         System.out.println("-----------------------------------------------");
-        System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "    |");
+        System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "   |");
         System.out.println("-----------------------------------------------");
-        System.out.println("1.Beli Barang");
-        System.out.println("2.Checkout");
-        System.out.println("3.Status pembayaran");
-        System.out.println("4.Update account");
-        System.out.println("0.Keluar");
-        System.out.println(" ");
+        System.out.println("| 1.Input data Barang                         |");
+        System.out.println("| 2.Update data Barang                        |");
+        System.out.println("| 3.Tampil Product                            |");
+        System.out.println("| 0.Keluar                                    |");
+        System.out.println("-----------------------------------------------");
+        System.out.println(" ");   
     }
+    
+    public void setProdukterjual(int Produk_terjual){
+        this.Produk_terjual = Produk_terjual;
+    }
+    
+    public void setSaldoPenjualan(int saldo_penjualan){
+        this.saldo_penjualan = saldo_penjualan;
+    }
+    
+    public int getProdukterjual(){
+        return Produk_terjual;
+    }
+    
+    public int getSaldoPenjualan(){
+        return saldo_penjualan;
+    }
+    
 }
