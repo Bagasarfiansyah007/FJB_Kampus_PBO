3da8483cae79462a3e6fd6633cc4c5ab2f4ea659
ridhoaanhrp <55271500+ridhoaanhrp@users.noreply.github.com>
2021-04-08 09:36:22 +0700

    Update Pembeli.java

--- Tugas_besar_FJB-Campus/src/Entitas/Pembeli.java
+++ Tugas_besar_FJB-Campus/src/Entitas/Pembeli.java
@@ -1,1 +1,22 @@
+package Entitas;
+import java.util.Scanner;
 
+
+public class Pembeli extends User{
+    private int saldo;
+    private int barangTerbeli;
+    
+    public void tampilMenuPembeli(User[] tableUser,int indeks){
+        System.out.println("-----------------------------------------------");
+        System.out.println("|            Login Sebagai Pembeli            |");
+        System.out.println("-----------------------------------------------");
+        System.out.println("|    Sukses login sebagai user : " + tableUser[indeks].getNim() + "    |");
+        System.out.println("-----------------------------------------------");
+        System.out.println("1.Beli Barang");
+        System.out.println("2.Checkout");
+        System.out.println("3.Status pembayaran");
+        System.out.println("4.Update account");
+        System.out.println("0.Keluar");
+        System.out.println(" ");
+    }
+}
